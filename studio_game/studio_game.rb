dd42fa330c18abebc794f6d1e4d5c6aba0033c97
Lazir Pascual <lpascual4579@conestogac.on.ca>
2024-05-21 19:26:39 -0400

    complete attributes exercise

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,4 +1,7 @@
 class Player
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
@@ -12,8 +15,12 @@ class Player
     @health += 15
   end
 
+  def score
+    @health + @name.length
+  end
+
   def to_s
-    "I'm #{@name} with a health of #{@health}"
+    "I'm #{@name} with a health of #{@health} and a score of #{score}"
   end
 
   # def to_s = "I'm #{@name} with a health of #{@health}"
@@ -39,4 +46,11 @@ player_4 = Player.new("alex", 125)
 puts player_4
 player_4.drain
 player_4.boost
+puts player_4
+
+puts player_4.name
+player_4.name = "Alexander"
+puts player_4.name
+puts player_4.health
+puts player_4.score
 puts player_4
