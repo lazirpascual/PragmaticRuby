2c0251d5497dcb49fc1649eed7a1b5ba0ff39958
Lazir Pascual <lpascual4579@conestogac.on.ca>
2024-05-22 18:14:13 -0400

    complete blocks exercise

--- studio_game/lib/studio_game/game.rb
+++ studio_game/lib/studio_game/game.rb
@@ -14,24 +14,28 @@ class Game
     rand(1..6)
   end
 
-  def play
+  def play(rounds = 1)
     puts "\nLet's play #{@title}!"
 
     puts "\nBefore playing:"
     puts @players
 
-    @players.each do |player|
-      number_rolled = roll_die
+    1.upto(rounds) do |round|
+      puts "\nRound #{round}:"
 
-      case number_rolled
-      when 1..2
-        player.drain
-        puts "#{player.name} got drained 😩"
-      when 3..4
-        puts "#{player.name} got skipped"
-      else
-        player.boost
-        puts "#{player.name} got boosted 😁"
+      @players.each do |player|
+        number_rolled = roll_die
+
+        case number_rolled
+        when 1..2
+          player.drain
+          puts "#{player.name} got drained 😩"
+        when 3..4
+          puts "#{player.name} got skipped"
+        else
+          player.boost
+          puts "#{player.name} got boosted 😁"
+        end
       end
     end
 
--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -11,14 +11,4 @@ game.add_player(player_1)
 game.add_player(player_2)
 game.add_player(player_3)
 game.add_player(player_4)
-game.play
-
-player_5 = Player.new("alvin", 100)
-player_6 = Player.new("simon", 60)
-player_7 = Player.new("theo", 125)
-
-chipmunks = Game.new("Chipmunks")
-chipmunks.add_player(player_5)
-chipmunks.add_player(player_6)
-chipmunks.add_player(player_7)
-chipmunks.play
+game.play(3)
