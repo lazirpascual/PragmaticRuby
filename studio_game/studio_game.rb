ef064dfc9c32cfa8bc386d1bc83f9c169cf3f5cb
Lazir Pascual <lpascual4579@conestogac.on.ca>
2024-05-22 16:55:03 -0400

    complete arrays exercise

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -27,26 +27,34 @@ class Player
 end
 
 player_1 = Player.new("finn", 60)
+player_2 = Player.new("lucy", 90)
+player_3 = Player.new("jase")
+player_4 = Player.new("alex", 125)
 
-number_rolled = rand(1..6)
+players = [player_1, player_2, player_3, player_4]
 
-# if number_rolled < 3
-#   player_1.drain
-#   puts "#{player_1.name} got drained 😩"
-# elsif number_rolled < 5
-#   puts "#{player_1.name} got skipped"
-# else
-#   player_1.boost
-#   puts "#{player_1.name} got boosted 😁"
-# end
+players.pop
 
-case number_rolled
-when 1..2
-  player_1.drain
-  puts "#{player_1.name} got drained 😩"
-when 3..4
-  puts "#{player_1.name} got skipped"
-else
-  player_1.boost
-  puts "#{player_1.name} got boosted 😁"
+player_5 = Player.new("cole", 75)
+players.push(player_5)
+
+puts "Before playing:"
+puts players
+
+players.each do |player|
+  number_rolled = rand(1..6)
+
+  case number_rolled
+  when 1..2
+    player.drain
+    puts "#{player.name} got drained 😩"
+  when 3..4
+    puts "#{player.name} got skipped"
+  else
+    player.boost
+    puts "#{player.name} got boosted 😁"
+  end
 end
+
+puts "\nAfter playing:"
+puts players
