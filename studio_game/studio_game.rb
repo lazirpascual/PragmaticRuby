3f933dd229fe1f3f3cf01aeb912a67243a82b787
Lazir Pascual <lpascual4579@conestogac.on.ca>
2024-05-23 18:12:35 -0400

    add player child classes

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,9 +1,17 @@
 require_relative "lib/studio_game/player"
 require_relative "lib/studio_game/game"
+require_relative "lib/studio_game/clumsy_player"
+require_relative "lib/studio_game/berserk_player"
 
 game = Game.new("Guardians")
 players_file = File.join(__dir__, "players.csv")
 game.load_players(ARGV.shift || players_file)
+
+clumsy = ClumsyPlayer.new("klutz", 105)
+game.add_player(clumsy)
+
+berserker = BerserkPlayer.new("berserker", 50)
+game.add_player(berserker)
 
 loop do
   print "\nHow many game rounds? ('quit' to exit) "
